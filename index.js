7afb4371a0158122caf16bbf285583226afe55c4
ShalomDauphin <shalomdauphin8@gmail.com>
2022-08-31 09:48:55 +0200

    Feat: CRUD Message

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const dbConnect = require("./config/dbConnect");
+const dbConnect = require('./config/dbConnect');
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 dbConnect();
-app.get("/", (req, res) => {
-  res.send('e-water-for-all')
+app.get('/', (req, res) => {
+	res.send('e-water-for-all');
 });
 
-app.use('/user', require('./routes/user'))
+app.use('/user', require('./routes/user'));
+app.use('/message', require('./routes/message'));
 
-mongoose.connection.once('open', ()=>{
-  console.log('DB connected')
-  app.listen(process.env.PORT || 3500,()=>{console.log(`server is running on port ${process.env.PORT}`)})
-})
+mongoose.connection.once('open', () => {
+	console.log('DB connected');
+	app.listen(process.env.PORT || 3500, () => {
+		console.log(`server is running on port ${process.env.PORT}`);
+	});
+});
